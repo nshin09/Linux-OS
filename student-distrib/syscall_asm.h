5ef2d9aa6737c59d28d20aba24fa05810e6a0b9f
Rockyd3 <legodaehler03@gmail.com>
2024-04-08 16:38:20 -0500

    Added syscall_asm function headers

--- student-distrib/syscall_asm.h
+++ student-distrib/syscall_asm.h
@@ -1,12 +1,17 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
+/* Calls the appropriate system called based on the passed number*/
 extern void syscall_handler();
 
+/* Saves the current context and then switches to a new program's context (Called in Execute)*/
 extern void Save_context(uint32_t ESP, uint32_t EIP);
 
+/* Gets register values so they can be read in C */
 extern uint32_t Get_EBP();
 extern uint32_t Get_ESP();
+
+/* Sets EBP and ESP and switches back to the original context (this func is called in halt)*/
 extern void Set_EBP_ESP(uint32_t EBP, uint32_t ESP);
 
 
